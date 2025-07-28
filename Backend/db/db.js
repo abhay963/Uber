188cbb904aaeab53f079e29a8c5bdd4668f23ed1
abhay963 <abhayyadav96312@gmail.com>
2025-07-29 00:37:18 +0530

    database connected

---
+++ Backend/db/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+function connectToDb() {
+    mongoose.connect(process.env.DB_CONNECT, {
+      
+        
+    })
+    .then(() => {
+        console.log('✅ Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('❌ MongoDB connection error:', err);
+    });
+}
+
+module.exports = connectToDb;
