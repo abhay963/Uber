7ae6f8959f2f96b0855ae00f716fe4149105dc39
abhay963 <abhayyadav96312@gmail.com>
2025-07-30 00:53:47 +0530

    added user routes

--- Backend/app.js
+++ Backend/app.js
@@ -3,13 +3,15 @@ const cors = require('cors');
 const express = require('express');
 
 dotenv.config(); // ✅ Load env variables
-
+const userRoutes=require('./routes/user.routes');
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json()); // ✅ Optional but good practice if working with JSON
+app.use(express.urlencoded({extended:true}));
 
+app.use('./users',userRoutes);
 
 const connectToDb=require('./db/db');
 connectToDb();
