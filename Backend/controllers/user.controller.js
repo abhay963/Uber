09ba15a4190e880a0d965076dfc867ff2543eb45
abhay963 <abhayyadav96312@gmail.com>
2025-07-30 00:54:13 +0530

    updated controller file

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,5 +1,26 @@
 const userModel=require('../models/user.model');
 const userService=require('../services/user.service');
+const {validationResult}=require('express-validator');
+
 module.exports.registerUser=async(req,res,next)=>{
+const errors=validationResult(req);
+if(!errors.isEmpty()){
+    return res.status(400).json({errors:errors.array()});
+}
+
+const {firstname,lastname,email,password}=req.body();
+
+const hashedPassword=await userModel.hashedPassword(password);
+
+
+const user=await userService.createUser({
+    firstname,
+    lastname,
+    email,
+    password:hashedPassword
+});
+
+const token=user.generateAuthtoken();
+res.status(201).json({token,user});
 
 }
